1b08ef0fd91484c1cde472b5c794489c1e0ab8d2
ryshino <ranature1153@gmail.com>
2023-01-22 16:29:59 +0900

    編集画面を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new  
   end
@@ -14,16 +23,37 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "ユーザー情報を更新しました"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   private
+    
+    def user_params
+      # adminを含んでいないのは任意のユーザーが
+      # 自分自身にアプリケーションの管理者権限を与えることを防止するため
+      params.require(:user).permit(:name, :profile, :password,
+                                  :password_confirmation, :profile_icon)
+    end
+    
+    # beforeフィルタ
 
-  def user_params
-    # adminを含んでいないのは任意のユーザーが
-    # 自分自身にアプリケーションの管理者権限を与えることを防止するため
-    params.require(:user).permit(:name, :profile, :password,
-                                :password_confirmation, :profile_icon)
-  end
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
+    end
 end
