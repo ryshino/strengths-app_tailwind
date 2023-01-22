017138f5bae7b06bde203584beda80baaced640f
ryshino <ranature1153@gmail.com>
2023-01-22 15:12:22 +0900

    新規登録画面と詳細画面を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new  
+  end
+
+  def create
+    @user = User.new(user_params)
+    # @user.profile_icon.attach(params[:user][:profile_icon])
+    if @user.save
+      flash[:success] = "ユーザー登録に成功しました"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    # adminを含んでいないのは任意のユーザーが
+    # 自分自身にアプリケーションの管理者権限を与えることを防止するため
+    params.require(:user).permit(:name, :profile, :password,
+                                :password_confirmation, :profile_icon)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 20 }
+  validates :profile, presence: true, uniqueness: true
+  validate :check_profile
+  #:allow_nilオプションは、対象の値がnilの場合にバリデーションをスキップする
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  has_secure_password
+  attr_accessor :remember_token
+
+  def check_profile
+    unless self.profile.start_with?("https://libecity.com/user_profile/")
+      errors.add(:base, "そのプロフィールURLは使用できません")
+    end
+  end
 end
