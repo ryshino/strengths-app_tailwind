a383894457e671bae909c297b88b84b6f69218b1
ryshino <ranature1153@gmail.com>
2023-01-25 07:48:42 +0900

    既存アプリでtailwindcss を導入できたので一旦コミットする

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,9 +1,24 @@
 class EpisodesController < ApplicationController
-  before_action :logged_in_user, only: [:index, :create, :destroy, :show]
+  before_action :logged_in_user, only: [:index, :new, :edit, :create, :destroy, :show]
   before_action :correct_user,   only: :destroy
 
   def index
     @episodes = Episode.all
+  end
+
+  def new
+    @episode = current_user.episodes.build
+  end
+
+  def create
+    @episode = current_user.episodes.build(episode_params)
+    if @episode.save
+      flash[:success] = "エピソードを投稿しました"
+      redirect_to episodes_path
+    else
+      flash.now[:success] = "エピソードを投稿できませんでした"
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   private
